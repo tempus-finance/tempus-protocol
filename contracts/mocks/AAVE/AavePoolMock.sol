e620bccd3975853e69e9eaa106038293a0f2778f
Jorma <jorma.rebane@gmail.com>
2021-06-08 15:35:25 +0300

    basic test for deposit()

--- contracts/mocks/AAVE/AavePoolMock.sol
+++ contracts/mocks/AAVE/AavePoolMock.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./ATokenMock.sol";
 
+// TODO: emit events matching with AAVE, these will be useful for frontend development
 contract AavePoolMock {
     ERC20 private backingToken;
     ATokenMock private yieldBearingToken;
@@ -22,11 +23,12 @@ contract AavePoolMock {
     /// Deposit an X amount of backing tokens into this pool as collateral
     /// and mint new ATokens in 1:1 ratio
     function deposit(uint256 amount) public {
-        require(
-            backingToken.balanceOf(msg.sender) >= amount,
-            "deposit: sender is too poor"
-        );
-        backingToken.approve(address(this), amount);
+        uint256 balance = backingToken.balanceOf(msg.sender);
+        require(balance >= amount, "deposit: sender is too poor");
+
+        uint256 allowance = backingToken.allowance(msg.sender, address(this));
+        require(allowance >= amount, "deposit: not enough allowance");
+
         backingToken.transferFrom(msg.sender, address(this), amount);
         yieldBearingToken.mint(msg.sender, amount);
     }
@@ -72,5 +74,14 @@ contract AavePoolMock {
     /// @return Total deposit of an user
     function getDeposit(address user) public view returns (uint256) {
         return yieldBearingToken.balanceOf(user);
+    }
+
+    function getReserveNormalizedIncome(address _underlyingAsset)
+        public
+        view
+        returns (uint256)
+    {
+        // TODO: not implemented
+        return 0;
     }
 }
--- test/aave.mock.test.ts
+++ test/aave.mock.test.ts
@@ -1,22 +1,24 @@
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
+import { expect } from "chai";
 
 describe("AAVE Mock", async () => {
   let pool;
+  let backingToken;
   let owner, addr1, addr2;
 
   beforeEach(async () => {
     [owner, addr1, addr2] = await ethers.getSigners();
 
     let BackingToken = await ethers.getContractFactory("ERC20FixedSupply");
-    let backingToken = await BackingToken.deploy("DAI Stablecoin", "DAI", 1000000);
+    backingToken = await BackingToken.deploy("DAI Stablecoin", "DAI", 1000000);
 
     let ATokenMock = await ethers.getContractFactory("ATokenMock");
     let yieldBearingToken = await ATokenMock.deploy('AAVE AToken', 'aDAI');
     let debtToken         = await ATokenMock.deploy('AAVE AToken', 'aDAI');
 
-    let AavePoolMock = ethers.getContractFactory("AavePoolMock");
-    pool = await (await AavePoolMock).deploy(
+    let AavePoolMock = await ethers.getContractFactory("AavePoolMock");
+    pool = await AavePoolMock.deploy(
       backingToken.address, 
       yieldBearingToken.address, 
       debtToken.address
@@ -27,7 +29,13 @@ describe("AAVE Mock", async () => {
   {
     it("Should deposit the correct amount", async () =>
     {
-      pool.connect(addr1).deposit(50);
+      backingToken.connect(owner).transfer(addr1.address, 10);
+
+      backingToken.connect(addr1).approve(pool.address, 1);
+      pool.connect(addr1).deposit(1);
+      
+      expect(await pool.getDeposit(addr1.address)).to.equal(1);
+      expect(await backingToken.balanceOf(addr1.address)).to.equal(9);
     });
   });
 });
