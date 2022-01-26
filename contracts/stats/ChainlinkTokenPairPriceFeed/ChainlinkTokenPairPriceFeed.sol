7b771419401365d04fb96e88c35b6674be694803
Yuval Weiss <yuval.weiss.ace@gmail.com>
2022-01-26 23:54:33 +0530

    [Stats] removed ENS support

--- contracts/stats/ChainlinkTokenPairPriceFeed/ChainlinkTokenPairPriceFeed.sol
+++ contracts/stats/ChainlinkTokenPairPriceFeed/ChainlinkTokenPairPriceFeed.sol
@@ -4,24 +4,10 @@ pragma solidity 0.8.10;
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import "./IChainlinkAggregator.sol";
-import "./IENS.sol";
-import "../ITokenPairPriceFeed.sol";
 
-abstract contract ChainlinkTokenPairPriceFeed is ITokenPairPriceFeed {
-    // The ENS registry (same for mainnet and all major testnets)
-    //
-    // See https://docs.chain.link/docs/ens/. This may need to be updated should Chainlink deploy
-    // on other networks with a different ENS address.
-    IENS private constant ENS = IENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
-
-    function getRate(bytes32 chainlinkAggregatorNodeHash)
-        public
-        view
-        override
-        returns (uint256 rate, uint256 rateDenominator)
-    {
-        IENSResolver ensResolver = ENS.resolver(chainlinkAggregatorNodeHash);
-        IChainlinkAggregator chainLinkAggregator = IChainlinkAggregator(ensResolver.addr(chainlinkAggregatorNodeHash));
+abstract contract ChainlinkTokenPairPriceFeed {
+    function getRate(address chainlinkAggregatorNode) public view returns (uint256 rate, uint256 rateDenominator) {
+        IChainlinkAggregator chainLinkAggregator = IChainlinkAggregator(chainlinkAggregatorNode);
 
         (, int256 latestRate, , , ) = chainLinkAggregator.latestRoundData();
 
