28eaaf8110d0f7626342eb19a0421580e6e626a5
Alex Beregszaszi <alex@rtfs.hu>
2022-02-09 23:01:21 +0100

    scripts: Add deployer script via config

--- scripts/deploy.pool.config.ts
+++ scripts/deploy.pool.config.ts
@@ -1,125 +1,240 @@
+import { writeFileSync, readFileSync } from 'fs';
 import { ethers, network } from 'hardhat';
+import { Contract } from '@ethersproject/contracts';
 import * as utils from './utils';
 import * as chalk from "chalk";
-import { generateTempusSharesNames } from '../test/utils/TempusPool';
+import { parseDecimal } from "../test/utils/Decimal";
+import { ERC20 } from "../test/utils/ERC20";
+import { AbstractSigner } from "../test/utils/ContractBase";
 
-
-const EXCHANGE_RATE_PRECISION = 18;
-const YBT_PRECISION = 18;
-const YBT_NAME = "Lido staked ETH";
-const YBT_SYMBOL = "stETH";
-const CONTRACT_NAME = "LidoTempusPool";
-const MONTH = 60 * 60 * 24 * 30;
-
-async function deploy() {
-  const deployerPrivateKey = await utils.promptPrivateKey("Enter deployer Private Key");
-
-  console.log(chalk.green(`* * * ${CONTRACT_NAME} configuration * * *`));
-  const lido = await utils.promptAddress("Enter the address of the Lido contract", (await utils.getDeployedContractAddress(network.name, 'Lido')));
-  const tempusController = await utils.promptAddress("Enter the address of the TempuController contract", (await utils.getDeployedContractAddress(network.name, 'TempusController')));
-  const maturityYear = await utils.promptNumber("Enter pool maturity year", null, 2021, 2025);
-  const maturityMonth = await utils.promptNumber("Enter pool maturity month", null, 1, 12);
-  const maturityDay = await utils.promptNumber("Enter pool maturity day", null, 1, 31);
-  const maturityHour = await utils.promptNumber("Enter pool maturity hour", null, 0, 23);
-  const maturityMinute = await utils.promptNumber("Enter pool maturity minute", null, 0, 59);
-  const estimatedYield = await utils.promptNumber("Enter estimated yield", null, 0.0001, 1);
-  const depositMaxFee = await utils.promptNumber("Enter maximum deposit fee", null, 0, 1);
-  const earlyRedemptionMaxFee = await utils.promptNumber("Enter maximum early redemption fee", null, 0, 1);
-  const maturedRedemptionMaxFee = await utils.promptNumber("Enter maximum matured redemption fee", null, 0, 1);
-  const referrer = await utils.promptAddress("Enter the address of the Lido Referrer");
-  
-  console.log(chalk.green(`* * * TempusAMM configuration * * *`));
-  const balancerVault = await utils.promptAddress("Enter the address of the Balancer Vault contract", (await utils.getDeployedContractAddress(network.name, 'Vault')));
-  const lpName = await utils.promptInput("Enter LP token Name", `Tempus ${YBT_SYMBOL} LP Token`);
-  const lpSymbol = await utils.promptInput("Enter LP token Symbol", `TLP${YBT_SYMBOL}`);
-  const amplificationStart = await utils.promptNumber("Enter initial Amplification Factor", 5, 1, 5000);
-  const amplificationEnd = await utils.promptNumber("Enter final Amplification Factor", 95, 1, 5000);
-  const swapFeePercentage = await utils.promptNumber("Enter Swap Fee percentage (e.g. - 0.02 to represent 2%)", 0.01, 0, 0.1);
-  const pauseWindowDuration = await utils.promptNumber("Enter Pause Window Duration (in seconds)", 3 * MONTH, 0);
-  const bufferPeriodDuration = await utils.promptNumber("Enter Buffer Period Duration (in seconds)", MONTH, 0);
-  const owner = await utils.promptAddress("Enter Owner address");
-
-  const maturityTimestamp = new Date(
-    Date.UTC(
-      maturityYear,
-      maturityMonth - 1, /// month range is 0-11 for some reason (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/UTC)
-      maturityDay,
-      maturityHour,
-      maturityMinute
-    )
-  ).getTime() / 1000;
-  
-  const sharesData = generateTempusSharesNames(YBT_NAME, YBT_SYMBOL, maturityTimestamp)
-  const poolConstructorArgs = [
-    lido,
-    tempusController,
-    maturityTimestamp,
-    ethers.utils.parseUnits(estimatedYield, EXCHANGE_RATE_PRECISION).toString(),
-    /*principalsData*/{
-      name: sharesData.principalName, 
-      symbol: sharesData.principalSymbol
-    },
-    /*yieldsData*/{
-      name: sharesData.yieldName, 
-      symbol: sharesData.yieldSymbol
-    },
-    /*maxFeeSetup:*/{
-      depositPercent:      ethers.utils.parseUnits(depositMaxFee, YBT_PRECISION).toString(),
-      earlyRedeemPercent:  ethers.utils.parseUnits(earlyRedemptionMaxFee, YBT_PRECISION).toString(),
-      matureRedeemPercent: ethers.utils.parseUnits(maturedRedemptionMaxFee, YBT_PRECISION).toString()
-    },
-    referrer
-  ];
-
-
-
-  console.log(chalk.yellow(`${CONTRACT_NAME} constructor arguments: `));
-  console.log(chalk.green(JSON.stringify(poolConstructorArgs)));
-  if (!(await utils.toggleConfirm("Do you confirm the constructor arguments?"))) {
-    console.log(chalk.yellow('Constructor arguments not confirmed.'));
-    process.exit(0)
-  }
-
-  console.log(chalk.yellow(`TempusAMM constructor arguments: `));
-  console.log(chalk.green(JSON.stringify({
-    balancerVault,
-    lpName,
-    lpSymbol,
-    amplificationStart,
-    amplificationEnd,
-    swapFeePercentage: ethers.utils.parseUnits(swapFeePercentage.toString(), 18).toString(),
-    pauseWindowDuration,
-    bufferPeriodDuration,
-    owner
-  })));
-  if (!(await utils.toggleConfirm("Do you confirm the constructor arguments?"))) {
-    console.log(chalk.yellow('Constructor arguments not confirmed.'));
-    process.exit(0)
-  }
-
-  const tempusPoolContract = await utils.deployContract(CONTRACT_NAME, poolConstructorArgs, deployerPrivateKey);
-  await utils.waitForContractToBeDeployed(tempusPoolContract.address);
-
-  const ammConstructorArgs = [
-    balancerVault,
-    lpName,
-    lpSymbol,
-    tempusPoolContract.address,
-    amplificationStart,
-    amplificationEnd,
-    ethers.utils.parseUnits(swapFeePercentage.toString(), 18).toString(),
-    pauseWindowDuration,
-    bufferPeriodDuration,
-    owner
-  ];
-
-  // Deploy AMM with a hardcoded 5.5M gas limit because otherwise gas estimation fails sometimes for some reason
-  const tempusAmmContract = await utils.deployContract("TempusAMM", ammConstructorArgs, deployerPrivateKey, 5500000);
-  await utils.waitForContractToBeDeployed(tempusAmmContract.address);
-  
-  await utils.generateDeployment(tempusPoolContract, `${CONTRACT_NAME}_${YBT_SYMBOL}_maturity-${maturityTimestamp}`, network.name);
-  await utils.generateDeployment(tempusAmmContract, `TempusAMM_${YBT_SYMBOL}_maturity-${maturityTimestamp}`, network.name);
+interface YBTConfig {
+  address: string;
+  decimals: number;
+  name: string;
+  symbol: string;
 }
 
+interface TokenNameSymbol {
+  name: string;
+  symbol: string;
+}
 
-deploy();
+interface FeesConfig {
+  deposit: number;
+  earlyRedemption: number;
+  maturedRedemption: number;
+}
+
+interface PoolConfig {
+  address?: string;
+  owner?: string;
+  maturity: string;
+  estimatedYield: string;
+  fees: FeesConfig;
+}
+
+interface AMMConfig {
+  address?: string;
+  vault: string;
+  lp: TokenNameSymbol;
+  owner: string;
+  swapFee: number;
+  pauseWindowDuration: number;
+  bufferPeriodDuration: number;
+  initialAmplificationFactor: number;
+  finalAmplificationFactor: number;
+}
+
+interface LidoConfig {
+  referrer: string;
+}
+
+interface RariConfig {
+  fundManager: string;
+}
+
+interface Config {
+  kind: string;
+  lido?: LidoConfig;
+  rari?: RariConfig;
+  decimals: number;
+  ybt: YBTConfig;
+  controller: string;
+  principal: TokenNameSymbol;
+  yield: TokenNameSymbol;
+  pool: PoolConfig;
+  amm: AMMConfig;
+}
+
+function validateRange(value:number, min:number, max:number) {
+  if (value < min) {
+    console.log(chalk.red(`Value ${value} below minimum of ${min}`));
+    process.exit(1);
+  } else if (value > max) {
+    console.log(chalk.red(`Value ${value} above maximum of ${max}`));
+    process.exit(1);
+  }
+}
+
+async function validateYBT(ybtConfig:YBTConfig, signer) {
+  const ybt = await ERC20.attachWithSigner("ERC20FixedSupply", ybtConfig.address, signer);
+
+  const decimals = ybt.decimals;
+  const name = await ybt.name();
+  const symbol = await ybt.symbol();
+
+  // Validate YBT details
+  if (ybtConfig.decimals !== decimals) {
+    console.log(chalk.red(`YBT: decimals mismatch: ${ybtConfig.decimals} vs ${decimals}`));
+    process.exit(1);
+  }
+  if (ybtConfig.name !== name) {
+    console.log(chalk.red(`YBT: name mismatch: ${ybtConfig.name} vs ${name}`));
+    process.exit(1);
+  }
+  if (ybtConfig.symbol !== symbol) {
+    console.log(chalk.red(`YBT: symbol mismatch: ${ybtConfig.symbol} vs ${symbol}`));
+    process.exit(1);
+  }
+}
+
+async function confirmAndDeploy(contractName:string, directory:string, label:string, args:any, deployerPrivateKey:string, gasLimit?:number): Promise<Contract> {
+  console.log(chalk.yellow(`${contractName} constructor arguments: `));
+  console.log(chalk.green(JSON.stringify(args)));
+  if (!(await utils.toggleConfirm("Do you confirm the constructor arguments?"))) {
+    console.log(chalk.yellow('Constructor arguments not confirmed.'));
+    process.exit(0)
+  }
+
+  const contractInstance = await utils.deployContract(contractName, args, deployerPrivateKey, gasLimit);
+  await utils.waitForContractToBeDeployed(contractInstance.address);
+
+  await utils.generateDeployment(contractInstance, label, directory);
+
+  return contractInstance;
+}
+
+async function deployPool(config:Config, deployerPrivateKey:string): Promise<Config> {
+  const provider = ethers.getDefaultProvider();
+  const signer = new ethers.Wallet(deployerPrivateKey).connect(provider);
+
+  await validateYBT(config.ybt, signer);
+
+  validateRange(config.decimals, 0, 33);
+  validateRange(config.ybt.decimals, 0, 33);
+  validateRange(Number(config.pool.estimatedYield), 0, 1);
+  validateRange(config.pool.fees.deposit, 0, 0.05);
+  validateRange(config.pool.fees.earlyRedemption, 0, 0.05);
+  validateRange(config.pool.fees.maturedRedemption, 0, 0.05);
+
+  let contractName;
+  let customPoolConstructorArgs = [];
+  if (config.kind === "Lido") {
+    contractName = "LidoTempusPool";
+    if (config.lido.referrer) {
+      customPoolConstructorArgs.push(config.lido.referrer);
+    }
+  } else if (config.kind === "Yearn") {
+    contractName = "YearnTempusPool";
+  } else if (config.kind === "Rari") {
+    contractName = "RariTempusPool";
+  } else {
+    console.log("No suitable protocol found");
+    process.exit(1);
+  }
+
+  const maturityTimestamp = Date.parse(config.pool.maturity) / 1000;
+  
+  const poolConstructorArgs = [
+    config.ybt.address,
+    config.controller,
+    maturityTimestamp,
+    parseDecimal(config.pool.estimatedYield, config.decimals),
+    /*principalsData*/{
+      name: config.principal.name,
+      symbol: config.principal.symbol
+    },
+    /*yieldsData*/{
+      name: config.yield.name,
+      symbol: config.yield.symbol
+    },
+    /*maxFeeSetup:*/{
+      depositPercent:      parseDecimal(config.pool.fees.deposit, config.ybt.decimals),
+      earlyRedeemPercent:  parseDecimal(config.pool.fees.earlyRedemption, config.ybt.decimals),
+      matureRedeemPercent: parseDecimal(config.pool.fees.maturedRedemption, config.ybt.decimals)
+    }
+  ].concat(customPoolConstructorArgs);
+
+  // NOTE: special case for Rari
+  if (config.kind === "Rari") {
+    // Insert fundManager as first argument
+    poolConstructorArgs.splice(0, 0, config.rari.fundManager);
+  }
+
+  const ybtSymbol = config.ybt.symbol;
+  const tempusPoolContract = await confirmAndDeploy(
+    contractName,
+    network.name,
+    `${contractName}_${ybtSymbol}_maturity-${maturityTimestamp}`,
+    poolConstructorArgs,
+    deployerPrivateKey
+  );
+
+  config.pool.address = tempusPoolContract.address;
+  config.pool.owner = await tempusPoolContract.owner();
+  return config;
+}
+
+async function deployAmm(config:Config, deployerPrivateKey:string): Promise<Config> {
+  validateRange(config.amm.initialAmplificationFactor, 0, 1000);
+  validateRange(config.amm.finalAmplificationFactor, 0, 1000);
+  validateRange(config.amm.swapFee, 0, 0.03);
+
+  const ammConstructorArgs = [
+    config.amm.vault,
+    config.amm.lp.name,
+    config.amm.lp.symbol,
+    config.pool.address,
+    config.amm.initialAmplificationFactor,
+    config.amm.finalAmplificationFactor,
+    parseDecimal(config.amm.swapFee, 18),
+    config.amm.pauseWindowDuration,
+    config.amm.bufferPeriodDuration,
+    config.amm.owner
+  ];
+
+  const ybtSymbol = config.ybt.symbol;
+  const maturityTimestamp = Date.parse(config.pool.maturity) / 1000;
+  // Deploy AMM with a hardcoded 5.5M gas limit because otherwise gas estimation fails sometimes for some reason
+  const tempusAmmContract = await confirmAndDeploy(
+    "TempusAMM",
+    network.name,
+    `TempusAMM_${ybtSymbol}_maturity-${maturityTimestamp}`,
+    ammConstructorArgs,
+    deployerPrivateKey,
+    5500000
+  );
+
+  config.amm.address = tempusAmmContract.address;
+  return config;
+}
+
+async function deploy(configName:string) {
+  let config:Config = JSON.parse(readFileSync(configName, 'utf-8').toString());
+
+  console.log(config);
+
+  const deployerPrivateKey = await utils.promptPrivateKey("Enter deployer Private Key");
+
+  if (config.pool.address === undefined) {
+    config = await deployPool(config, deployerPrivateKey);
+  }
+
+  if (config.amm.address === undefined) {
+    config = await deployAmm(config, deployerPrivateKey);
+  }
+
+  writeFileSync(configName, JSON.stringify(config, null, 2));
+}
+
+deploy(process.env.TEMPUS_POOL_JSON);
