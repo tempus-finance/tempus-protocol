d11d10f7af50420d276f14b821847e2535e8ae8e
Alex Beregszaszi <alex@rtfs.hu>
2022-04-07 13:49:48 +0200

    amm: Remove unused functions from amm/math/Math

--- contracts/amm/math/Math.sol
+++ contracts/amm/math/Math.sol
@@ -6,20 +6,6 @@ pragma solidity 0.8.10;
  * Adapted from OpenZeppelin's SafeMath library
  */
 library Math {
-    /**
-     * @dev Returns the largest of two numbers of 256 bits.
-     */
-    function max(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a >= b ? a : b;
-    }
-
-    /**
-     * @dev Returns the smallest of two numbers of 256 bits.
-     */
-    function min(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-
     function div(
         uint256 a,
         uint256 b,
