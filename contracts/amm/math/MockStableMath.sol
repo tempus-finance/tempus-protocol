3b7f18c9b107c241dbd6e074150bb7a723de4d20
RedFox <jorma.rebane@gmail.com>
2022-04-11 12:13:17 +0300

    refactor: #580 rename BPT to LP in TempusAMM / StableMath contracts

--- contracts/amm/math/MockStableMath.sol
+++ contracts/amm/math/MockStableMath.sol
@@ -30,70 +30,70 @@ contract MockStableMath {
         return StableMath.inGivenOut(amp, balances[0], balances[1], firstTokenOut, tokenAmountOut);
     }
 
-    function bptOutGivenTokensIn(
+    function lpOutGivenTokensIn(
         uint256 amp,
         uint256[] memory balances,
         uint256[] memory amountsIn,
-        uint256 bptTotalSupply,
+        uint256 lpTotalSupply,
         uint256 swapFeePercentage
     ) external pure returns (uint256) {
         return
-            StableMath.bptOutGivenTokensIn(
+            StableMath.lpOutGivenTokensIn(
                 amp,
                 balances[0],
                 balances[1],
                 amountsIn[0],
                 amountsIn[1],
-                bptTotalSupply,
+                lpTotalSupply,
                 swapFeePercentage
             );
     }
 
-    function bptInGivenTokensOut(
+    function lpInGivenTokensOut(
         uint256 amp,
         uint256[] memory balances,
         uint256[] memory amountsOut,
-        uint256 bptTotalSupply,
+        uint256 lpTotalSupply,
         uint256 swapFeePercentage
     ) external pure returns (uint256) {
         return
-            StableMath.bptInGivenTokensOut(
+            StableMath.lpInGivenTokensOut(
                 amp,
                 balances[0],
                 balances[1],
                 amountsOut[0],
                 amountsOut[1],
-                bptTotalSupply,
+                lpTotalSupply,
                 swapFeePercentage
             );
     }
 
-    function tokenOutFromBptIn(
+    function tokenOutFromLPIn(
         uint256 amp,
         uint256[] memory balances,
         bool firstToken,
-        uint256 bptAmountIn,
-        uint256 bptTotalSupply,
+        uint256 lpAmountIn,
+        uint256 lpTotalSupply,
         uint256 swapFee
     ) external pure returns (uint256) {
         return
-            StableMath.tokenOutFromBptIn(
+            StableMath.tokenOutFromLPIn(
                 amp,
                 balances[0],
                 balances[1],
                 firstToken,
-                bptAmountIn,
-                bptTotalSupply,
+                lpAmountIn,
+                lpTotalSupply,
                 swapFee
             );
     }
 
-    function tokensOutFromBptIn(
+    function tokensOutFromLPIn(
         uint256[] memory balances,
-        uint256 bptAmountIn,
-        uint256 bptTotalSupply
+        uint256 lpAmountIn,
+        uint256 lpTotalSupply
     ) external pure returns (uint256 amountOut0, uint256 amountOut1) {
-        return StableMath.tokensOutFromBptIn(balances[0], balances[1], bptAmountIn, bptTotalSupply);
+        return StableMath.tokensOutFromLPIn(balances[0], balances[1], lpAmountIn, lpTotalSupply);
     }
 
     function getTokenBalance(
