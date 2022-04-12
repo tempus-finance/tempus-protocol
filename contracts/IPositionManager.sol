a8e4c581001cf1c50b35ee937437c3f2323d6fb4
Yuval Weiss <yuval.weiss.ace@gmail.com>
2022-04-12 15:54:51 +0300

    added PositionManager

---
+++ contracts/IPositionManager.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./amm/interfaces/ITempusAMM.sol";
+import "./ITempusPool.sol";
+
+interface IPositionManager is IERC721, IERC721Metadata {
+    /// @dev Provided Leverage Multiplier is invalid
+    error InvalidLeverageMultiplier();
+    /// @dev Unauthorized position burn attempt
+    error UnauthorizedBurn();
+    /// @dev A Tempus AMM whose shares are of different TempusPools was provided
+    error AmmSharesPoolMismatch();
+
+    /// @dev Holds information about an open position
+    /// @param capitals Amount of Capitals owned by the position
+    /// @param yields Amount of Yields owned by the position
+    /// @param tempusAMM The Tempus AMM used to create the position
+    struct Position {
+        uint128 capitals;
+        uint128 yields;
+        ITempusAMM tempusAMM;
+    }
+
+    /// @param tempusAMM the Tempus AMM (and its Tempus Pool) to use for position creation
+    /// @param leverageMultiplier For fixed positions, use leverageMultiplier = 0; For levarged positions, use leverageMultiplier > 1e18
+    /// @param tokenAmountToDeposit Token amount to be deposited intto the Tempus Pool
+    /// @param worstAcceptableCapitalsRate Worst acceptable Capitals rate for the internal Capitals <--> Yields swap
+    /// @param deadline A timestamp by which, if a swap is necessary, the transaction must be completed
+    /// @param recipient Recipient to be granted ownership of the position
+    /// @param isBackingToken Specfies whether to deposit Backing Tokens or Yield Bearing Tokens
+    struct MintParams {
+        ITempusAMM tempusAMM;
+        uint256 leverageMultiplier;
+        uint256 tokenAmountToDeposit;
+        uint256 worstAcceptableCapitalsRate;
+        uint256 deadline;
+        address recipient;
+        bool isBackingToken;
+    }
+
+    /// @param maxLeftoverShares Maximum amount of Principals/Yields to be left in case early exit swap is necessary
+    /// @param yieldsRate Base exchange rate of Yields (denominated in Capitals)
+    /// @param maxSlippage Maximum allowed change in the exchange rate from the base yieldsRate (1e18 precision)
+    /// @param deadline A timestamp by which, if a swap is necessary, the transaction must be completed
+    /// @param recipient Recipient to be receive the funds received from position liquidation
+    /// @param toBackingToken Specfies whether to withdraw to Backing Tokens or Yield Bearing Tokens
+    struct BurnParams {
+        uint256 maxLeftoverShares;
+        uint256 yieldsRate;
+        uint256 maxSlippage;
+        uint256 deadline;
+        address recipient;
+        bool toBackingToken;
+    }
+
+    /// @dev Deposits funds into a provided TempusPool and mints
+    ///      a non-fungible token that represents that position
+    /// @notice this function can be used to mint both fixed rate positions and leveraged positions.
+    ///         to mint a leveraged position, use a leverageMultiplier that is greater than 1e18;
+    ///         to mint a fixed rate position, use 0 as the leverageMultiplier.
+    /// @param params Instructions for position creation
+    /// @return tokenId of the minted position
+    function mint(MintParams calldata params) external payable returns (uint256 tokenId);
+
+    /// @dev Burns a non-fungible positions and liquidates its value to the position owner
+    /// @param tokenId Token ID to burn
+    /// @param params Instructions for position liquidation
+    function burn(uint256 tokenId, BurnParams calldata params) external;
+
+    /// @dev retrieves position data for a given token ID.
+    function position(uint256 tokenId) external view returns (Position memory);
+}
---
+++ contracts/PositionManager.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+import "./ITempusController.sol";
+import "./IPositionManager.sol";
+import "./utils/UntrustedERC20.sol";
+import "./math/Fixed256xVar.sol";
+
+contract PositionManager is IPositionManager, ERC721, ReentrancyGuard {
+    using Fixed256xVar for uint256;
+    using SafeERC20 for IERC20;
+    using UntrustedERC20 for IERC20;
+
+    mapping(uint256 => Position) private _positions;
+    uint256 private _nextId = 1;
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function mint(MintParams calldata params) external payable override nonReentrant returns (uint256 tokenId) {
+        ITempusPool tempusPool = params.tempusAMM.token0().pool();
+        if (tempusPool != params.tempusAMM.token1().pool()) {
+            revert AmmSharesPoolMismatch();
+        }
+
+        address controller = tempusPool.controller();
+
+        uint256 tokenAmountToDeposit = msg.value;
+        {
+            address depositedAsset = params.isBackingToken ? tempusPool.backingToken() : tempusPool.yieldBearingToken();
+
+            if (depositedAsset != address(0)) {
+                tokenAmountToDeposit = IERC20(depositedAsset).untrustedTransferFrom(
+                    msg.sender,
+                    address(this),
+                    params.tokenAmountToDeposit
+                );
+                IERC20(depositedAsset).safeIncreaseAllowance(controller, tokenAmountToDeposit);
+            }
+        }
+
+        uint256 capitalsMinted;
+        uint256 yieldsMinted;
+        if (params.leverageMultiplier == 0) {
+            uint256 backingTokenONE = tempusPool.backingTokenONE();
+            capitalsMinted = ITempusController(controller).depositAndFix{value: msg.value}(
+                params.tempusAMM,
+                tempusPool,
+                tokenAmountToDeposit,
+                params.isBackingToken,
+                backingTokenONE.divfV(params.worstAcceptableCapitalsRate, backingTokenONE),
+                params.deadline
+            );
+        } else if (params.leverageMultiplier > 1e18) {
+            (capitalsMinted, yieldsMinted) = ITempusController(controller).depositAndLeverage{value: msg.value}(
+                params.tempusAMM,
+                tempusPool,
+                params.leverageMultiplier,
+                tokenAmountToDeposit,
+                params.isBackingToken,
+                params.worstAcceptableCapitalsRate,
+                params.deadline
+            );
+        } else {
+            revert InvalidLeverageMultiplier();
+        }
+
+        tokenId = _nextId++;
+        _positions[tokenId] = Position({
+            capitals: SafeCast.toUint128(capitalsMinted),
+            yields: SafeCast.toUint128(yieldsMinted),
+            tempusAMM: params.tempusAMM
+        });
+
+        _safeMint(params.recipient, tokenId);
+    }
+
+    function burn(uint256 tokenId, BurnParams calldata params) external override nonReentrant {
+        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
+            revert UnauthorizedBurn();
+        }
+
+        Position memory p = _positions[tokenId];
+
+        delete _positions[tokenId];
+        _burn(tokenId);
+
+        ITempusPool tempusPool = p.tempusAMM.token0().pool();
+        uint256 liquidatedTokenAmount = _liquidatePosition(
+            p.tempusAMM,
+            tempusPool,
+            p.capitals,
+            p.yields,
+            params.maxLeftoverShares,
+            params.yieldsRate,
+            params.maxSlippage,
+            params.toBackingToken,
+            params.deadline
+        );
+
+        if (params.toBackingToken) {
+            IERC20(tempusPool.backingToken()).safeTransfer(params.recipient, liquidatedTokenAmount);
+        } else {
+            IERC20(tempusPool.yieldBearingToken()).safeTransfer(params.recipient, liquidatedTokenAmount);
+        }
+    }
+
+    function position(uint256 tokenId) external view override returns (Position memory) {
+        return _positions[tokenId];
+    }
+
+    function _liquidatePosition(
+        ITempusAMM amm,
+        ITempusPool tempusPool,
+        uint128 capitals,
+        uint128 yields,
+        uint256 maxLeftoverShares,
+        uint256 yieldsRate,
+        uint256 maxSlippage,
+        bool toBackingToken,
+        uint256 deadline
+    ) private returns (uint256 liquidatedTokenAmount) {
+        address controller = tempusPool.controller();
+        IERC20(address(tempusPool.principalShare())).approve(controller, capitals);
+        IERC20(address(tempusPool.yieldShare())).approve(controller, yields);
+        liquidatedTokenAmount = ITempusController(controller).exitAmmGivenLpAndRedeem(
+            amm,
+            tempusPool,
+            0, // lpTokens is 0 since LP tokens are not supported by the PositionManager
+            capitals,
+            yields,
+            0, // minPrincipalsStaked is 0 since LP tokens are not supported by the PositionManager
+            0, // minYieldsStaked is 0 since LP tokens are not supported by the PositionManager
+            maxLeftoverShares,
+            yieldsRate,
+            maxSlippage,
+            toBackingToken,
+            deadline
+        );
+    }
+}
