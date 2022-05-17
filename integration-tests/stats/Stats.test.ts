bf0eb30e4e268588600e62e01714054b1b3b1674
Jorma <jorma.rebane@gmail.com>
2022-05-18 00:48:47 +0300

    test: refactored integration-tests for `Stats`

--- integration-tests/stats/Stats.test.ts
+++ integration-tests/stats/Stats.test.ts
@@ -1,18 +1,14 @@
 import { expect } from "chai";
-import {
-  ethers,
-  deployments,
-  getNamedAccounts
-} from 'hardhat';
-import { BigNumber } from '@ethersproject/bignumber';
-import * as NameHash from 'eth-ens-namehash';
+import { ethers, deployments } from 'hardhat';
 import { blockTimestamp } from '../../test/utils/Utils';
 import { generateTempusSharesNames, TempusPool } from "../../test/utils/TempusPool";
 import { ERC20 } from "../../test/utils/ERC20";
 import { ContractBase } from "../../test/utils/ContractBase";
-import { toWei } from '../../test/utils/Decimal';
+import { decimal, toWei } from '../../test/utils/Decimal';
 import { EthPriceQuoteProvider } from '../EthPriceQuoteProvider';
 import { TempusController } from "../../test/utils/TempusController";
+import { describeNonPool } from "../../test/pool-utils/MultiPoolTestSuite";
+import { getAccounts } from "../IntegrationUtils";
 
 if (!process.env.HARDHAT_FORK_NUMBER) {
   throw new Error('HARDHAT_FORK_NUMBER env var is not defined');
@@ -20,63 +16,45 @@ if (!process.env.HARDHAT_FORK_NUMBER) {
 const FORKED_BLOCK_NUMBER = Number(process.env.HARDHAT_FORK_NUMBER);
 
 const setup = deployments.createFixture(async () => {
-  await deployments.fixture(undefined, {
-    keepExistingDeployments: true, // global option to test network like that
-  });
+  await deployments.fixture(undefined, { keepExistingDeployments: true });
 
-  const owner = (await ethers.getSigners())[0];
-  const { aWethHolder } = await getNamedAccounts();
-  
-  const aWethHolderSigner = await ethers.getSigner(aWethHolder);
-  
+  const { owner, holder } = await getAccounts('aWethHolder');
+
   const Weth = new ERC20("ERC20", 18, (await ethers.getContract("Weth")));
-  const aWethYieldToken = new ERC20("ERC20", 18, (await ethers.getContract('aToken_Weth')));
-  
-  const maturityTime = await blockTimestamp() + 60*60; // maturity is in 1hr
+  const aWeth = new ERC20("ERC20", 18, (await ethers.getContract('aToken_Weth')));
 
+  const maturityTime = await blockTimestamp() + 60*60; // maturity is in 1hr
   const names = generateTempusSharesNames("Aave wrapped ether", "aWETH", maturityTime);
   const yieldEst = 0.1;
   const controller = await TempusController.deploy(owner);
   const tempusPool = await TempusPool.deployAave(
-    owner, Weth, aWethYieldToken, controller, maturityTime, yieldEst, names
+    owner, Weth, aWeth, controller, maturityTime, yieldEst, names
   );
-  
-  const stats = await ContractBase.deployContract("Stats");
 
-  return {
-    contracts: {
-      tempusPool,
-      controller,
-      aWeth: aWethYieldToken,
-      stats
-    },
-    signers: {
-      aWethHolder: aWethHolderSigner
-    }
-  };
+  const stats = await ContractBase.deployContract("Stats");
+  return { contracts: { tempusPool, controller, aWeth, stats }, signers: { aWethHolder: holder } };
 });
 
-describe('Stats <> Chainlink', function () {
+describeNonPool('Stats <> Chainlink', () => {
   it('verifies querying the TVL of a pull in USD denominations returns a correct result', async () => {
     // arrange
     const { signers: { aWethHolder }, contracts: { aWeth, controller, tempusPool, stats }} = await setup();
-    const depositAmount: number = 1234.56789;
-    
+    const depositAmount = 1234.56789;
+
     // https://docs.chain.link/docs/ethereum-addresses/
     const chainlinkAggregatorNode = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"; 
 
     const currentBlockDate = new Date(1000 * (await ethers.provider.getBlock(FORKED_BLOCK_NUMBER)).timestamp);
     const ethPriceQuote = await EthPriceQuoteProvider.getDailyQuote(currentBlockDate);
-    
+
     // act
     await aWeth.approve(aWethHolder, tempusPool.address, depositAmount);
     await controller.depositYieldBearing(aWethHolder, tempusPool, depositAmount, aWethHolder);
-    
-    // assert
-    const totalValueLockedInUSD :BigNumber = await stats.totalValueLockedAtGivenRate(tempusPool.address, chainlinkAggregatorNode);
-    const minExpectedTotalValueLockedInUSD = Number(toWei(depositAmount).mul(toWei(ethPriceQuote.low)).div(toWei(1)));
-    const maxExpectedTotalValueLockedInUSD = Number(toWei(depositAmount).mul(toWei(ethPriceQuote.high)).div(toWei(1)));
 
-    expect(Number(totalValueLockedInUSD)).to.be.within(minExpectedTotalValueLockedInUSD, maxExpectedTotalValueLockedInUSD);
+    // assert
+    const totalValueLockedInUSD = +decimal(await stats.totalValueLockedAtGivenRate(tempusPool.address, chainlinkAggregatorNode));
+    const minExpectedTVLInUSD = +decimal(depositAmount).mul(ethPriceQuote.low);
+    const maxExpectedTVLInUSD = +decimal(depositAmount).mul(ethPriceQuote.high);
+    expect(totalValueLockedInUSD).to.be.within(minExpectedTVLInUSD, maxExpectedTVLInUSD);
   });
-})
+});
