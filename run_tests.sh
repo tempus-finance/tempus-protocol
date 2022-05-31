11ccf9e55163279a45ac81680bb8dcb39918841b
Jorma <jorma.rebane@gmail.com>
2022-05-31 17:28:53 +0300

    ci: disable parallel testing for integration-tests due to constant rate limit issues

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,7 @@ TESTS_PATH=""
 HARDHAT_FORK="mainnet"
 HARDHAT_FORK_NUMBER="13281002"
 ENVS=""
+PARALLEL=true
 # not all progress reporters work well with multi-process output
 # those that cache results in a single file are the worst
 REPORTER="progress"
@@ -39,6 +40,8 @@ while [ -n "$1" ]; do
     TESTS_PATH="integration-tests"
     ONLY_TOKEN="all" # handle all tokens in a single process instead of spawning multiple
 
+  elif [ "$1" == "no-parallel" ]; then # disable parallelization
+    PARALLEL=false
   elif [[ "$POOLS" =~ "$1" ]]; then # only pool
     ONLY_POOL="$1"
   elif [[ "$VALID_TOKENS" =~ "$1" ]]; then # only token
@@ -60,38 +63,43 @@ fi
 
 start_time=$(date +%s)
 
-PIDS=()
-for pool in $pools; do
-  tokens=${POOL_TOKENS[$pool]}
-  if [ "$ONLY_TOKEN" ]; then
-    if [[ "$VALID_TOKENS" =~ "$ONLY_TOKEN" ]]; then
-      tokens="$ONLY_TOKEN"
-    else # ONLY_TOKEN is not a valid token for this pool
-      echo Skipping $ONLY_TOKEN for $pool - $ONLY_TOKEN
-      continue
+if [ "$PARALLEL" == "true" ]; then
+  PIDS=()
+  for pool in $pools; do
+    tokens=${POOL_TOKENS[$pool]}
+    if [ "$ONLY_TOKEN" ]; then
+      if [[ "$VALID_TOKENS" =~ "$ONLY_TOKEN" ]]; then
+        tokens="$ONLY_TOKEN"
+      else # ONLY_TOKEN is not a valid token for this pool
+        echo Skipping $ONLY_TOKEN for $pool - $ONLY_TOKEN
+        continue
+      fi
     fi
-  fi
-  for token in $tokens; do
-    echo Start Tests $pool - $token
-    cross-env ONLY_POOL=$pool ONLY_TOKEN=$token $ENVS $CMD &
-    PIDS+=($!)
+    for token in $tokens; do
+      echo Start Parallel Test $pool - $token
+      cross-env ONLY_POOL=$pool ONLY_TOKEN=$token $ENVS $CMD &
+      PIDS+=($!)
+    done
   done
-done
 
-# and finally run the non-pool tests, only if we haven't set ONLY_POOL or ONLY_TOKEN
-if [ "$ONLY_POOL" == "" ] && [ "$ONLY_TOKEN" == "" ]; then
-  echo Start Tests Non-Pool
-  cross-env ONLY_POOL=None $ENVS $CMD &
-  PIDS+=($!)
+  # and finally run the non-pool tests, only if we haven't set ONLY_POOL or ONLY_TOKEN
+  if [ "$ONLY_POOL" == "" ] && [ "$ONLY_TOKEN" == "" ]; then
+    echo Start Tests Non-Pool
+    cross-env ONLY_POOL=None $ENVS $CMD &
+    PIDS+=($!)
+  fi
+
+  # wait for results and check for failure
+  FAILS=0
+  TOTAL=0
+  for pid in "${PIDS[@]}"; do
+    let "TOTAL+=1"
+    wait $pid || let "FAILS+=1"
+  done
+else
+  echo Start Unparallelized Test $ONLY_POOL - $ONLY_TOKEN
+  cross-env ONLY_POOL=$ONLY_POOL ONLY_TOKEN=$ONLY_TOKEN $ENVS $CMD
 fi
-
-# wait for results and check for failure
-FAILS=0
-TOTAL=0
-for pid in "${PIDS[@]}"; do
-  let "TOTAL+=1"
-  wait $pid || let "FAILS+=1"
-done
 
 end_time=$(date +%s)
 elapsed=$(( end_time - start_time ))
